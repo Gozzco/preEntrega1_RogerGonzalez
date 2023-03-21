7fe68d7f8c0127a4ce91a7dde9fd680debeab04d
Gozzco <roger.a.gonz@gmail.com>
2023-03-20 22:12:20 -0300

    Changes to be committed: 	modified:   script.js

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
-/* Se le da la bienvenida al usuario y se le invita a indicar su nombre, mail y dirección. Si todos los campos fueron completados se le indica al usaurio Gracias *nombre* los productos que compres serán enviados a *dirección*. En caso contrario se le indicara que debe completar todos los campos solicitados*/
+/* Se le da la bienvenida al usuario y se le invita a indicar su nombre, mail y dirección. Si todos los campos fueron completados se le indica al usuario "Gracias *nombre* los productos que compres serán enviados a *dirección*"". En caso contrario (vacíos o null) se le indicara que debe completar todos los campos solicitados*/
 
+
+
+let nombre, mail, calle, número, depto;
 
 do{
-    let nombre = prompt ("Hola! Bienvenido a Needo, cómo te llamas?");
-    let mail = prompt ("Por favor indicanos tu mail");
-    let calle = prompt ("Indicanos la calle de tu domicilio");
-    let numero = Number (prompt ("A qué altura?"));
-    let depto = prompt ("Indicanos piso y número de departamento");
+    nombre = prompt ("Hola! Bienvenido a Needo, cómo te llamas?");
+    mail = prompt ("Por favor indicanos tu mail");
+    calle = prompt ("Indicanos la calle de tu domicilio");
+    numero = Number (prompt ("A qué altura?"));
+    depto = prompt ("Indicanos piso y número de departamento");
 
-    if ((nombre != "") && (mail != "") && (calle != "") && (numero != "") && (depto != "")) {
-        let perfil = "Bienvenido, " + nombre + "! " + "Registramos tu mail " + mail + "\n" + "La dirección de entrega que indicaste es: " + calle + ", " + numero + " " + depto
+    if ((nombre === "" || nombre === null) || (mail === "" || mail === null) || (calle === "" || calle === null) || (numero === "" || numero === null) || (depto === "" || depto === null) || (isNaN(numero))){
+
+        alert("Por favor completa todos los datos solicitados y el número de la calle debe ser un número")
+
+    }else{
+        let perfil = "Bienvenido, " + nombre + "! " + "Registramos tu mail " + mail + ". " + "La dirección de entrega que indicaste es: " + "calle " + calle + ", altura " + numero + ", departamento " + depto
 
         alert (perfil)
         console.log (perfil)
-    }else{
-    alert("Por favor completa todos los datos solicitados")
     }
 }while (confirm ("Deseas corregir algún dato?")){
-    if (confirm == true) {
+    
+    if (confirm === true) {
         nombre = prompt ("Hola! Bienvenido a Needo, cómo te llamas?");
     }else{
-        alert ("Muchas gracias por registrarte!")
+        alert ("Muchas gracias!")
     }
 }
